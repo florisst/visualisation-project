b7d3e76dc66ca87eced623d6cf68eb8202540b70
Floris Stevens <florisst@live.nl>
2015-06-04 16:57:47 +0200

    start UI

---
+++ code/WillemAlexander.js
@@ -0,0 +1,13 @@
+
+function load(){
+	console.log("hi")
+	console.log(hi)
+	var width = 960,
+    height = 1160;
+	var svg = d3.select("body").append("svg")
+    	.attr("width", width)
+    	.attr("height", height)
+    	.attr("opacity", "1");
+
+    	console.log("svg")
+}
