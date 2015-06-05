fcc7ef280e275481dc90024586468dc514d69596
Floris Stevens <florisst@live.nl>
2015-06-05 10:26:17 +0200

    Eerste versie user interface

--- code/WillemAlexander.js
+++ code/WillemAlexander.js
@@ -1,13 +1,3 @@
+window.onload = function(){
 
-function load(){
-	console.log("hi")
-	console.log(hi)
-	var width = 960,
-    height = 1160;
-	var svg = d3.select("body").append("svg")
-    	.attr("width", width)
-    	.attr("height", height)
-    	.attr("opacity", "1");
-
-    	console.log("svg")
 }
