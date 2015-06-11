d5b0ba7f6e7658082227ed72f337674eedc5a4fc
Floris Stevens <florisst@live.nl>
2015-06-11 12:37:20 +0200

    Tooltip voor hele heat functioneel

--- code/WillemAlexander.js
+++ code/WillemAlexander.js
@@ -77,17 +77,17 @@ function loadDamen2014(){
 
 // name variables and use those for visualisation.
 function westelijke2015(error, data_array){
-	results = data_array[0]
+  results = data_array[0]
 	weather = data_array[1]
 	selectDay(results, weather)
 }
 function damen2015(error, data_array){
-	results = data_array[0]
+  results = data_array[0]
 	weather = data_array[1]
   selectDay(results, weather);
 }
 function damen2014(error, data_array){
-	results = data_array[0]
+  results = data_array[0]
 	weather = data_array[1]
   selectDay(results, weather);
 }
@@ -95,14 +95,17 @@ function damen2014(error, data_array){
 // Only select those heats that where rowed on Saturday 
 function saturday (results, weather){
   day_results = []
+  clearDayResults(day_results);
+  day_results = []
   d3.json(results, function() {
     results.forEach(function(d) {
       d.temp = d[0]
       if (d.temp == "za" || d.temp == "za ")
-        day_results.push(d)
+          day_results.push(d)
     });
-    console.log(day_results)
+    //console.log(day_results.length)
     clearSVG();
+    initialise();
     drawScatter(day_results, weather)
   })
 }
@@ -115,10 +118,16 @@ function sunday (results, weather){
       if (d.temp == "zo" || d.temp == "zo ")
         day_results.push(d)
     });
-    console.log(day_results)
     clearSVG();
+    initialise();
     drawScatter(day_results, weather)
   })
+}
+function clearDayResults(day_results){
+  while(day_results.length > 0)
+  day_results.pop();
+  console.log(day_results.length)
+  return day_results;
 }
 
 // Draw the actual graph.
@@ -129,23 +138,7 @@ function drawScatter(day_results, weather){
   height = 600  - margin.top - margin.bottom;
   var padding = 50;
   var left_pad = 100;
-  initialise();
 
-  // create the X and Y axis.
-	var x = d3.time.scale()
-    	.range([padding, width - padding]);
-	var y = d3.scale.linear()
-    	.range([padding, height - padding*2]);
-	var xAxis = d3.svg.axis()
-    	.scale(x)
-    	.orient("bottom");
-	var yAxis = d3.svg.axis()
-    	.scale(y)
-      .orient("right")
-      .ticks(6)
-        .tickFormat(function (d, i) {
-            return ['1', '2', '3', '4', '5', '6', '7', '8'][d];
-        });
   var scatter_results = d3.select("svg")
   // reach the data with d3.
   var parseTime = d3.time.format("%H:%M").parse;
@@ -154,14 +147,32 @@ function drawScatter(day_results, weather){
     d.baan = d[4]
 		d.startTijd = parseTime(d[1])
 		d.crew = d[5]
+    d.finishtime = d.crew["results"]["finish"]["time"]
     d.finishPos = d.crew["results"]["finish"]["position"]
     d.radius = 20-3*d.finishPos
+    d.heat = d[2]
+    d.time500 = d.crew["results"]["500m"]["time"]
+    d.pos500 = d.crew["results"]["500m"]["position"]
+    d.time1000 = d.crew["results"]["1000m"]["time"]
+    d.pos1000 = d.crew["results"]["1000m"]["position"]
+    d.time1500 = d.crew["results"]["1500m"]["time"]
+    d.pos1500 = d.crew["results"]["1500m"]["position"]
+    d.crewname = d[3]
+    
     // Other variables to contain the data still need to be made.
     });
+  var xDomain = d3.extent(day_results, function(d) { return d.startTijd; })
+  var yDomain = d3.extent(day_results, function(d) { return d.baan; });
 
-	// Scale the data to the SVG.
-  x.domain(d3.extent(day_results, function(d) { return d.startTijd; })).nice();
-  y.domain(d3.extent(day_results, function(d) { return d.baan; })).nice();
+  var xScale = d3.time.scale().range([padding, width - padding]).domain(xDomain);
+  var yScale = d3.scale.linear().range([padding, height - padding*2]).domain(yDomain);
+
+  var xAxis = d3.svg.axis().scale(xScale).orient('bottom');
+  var yAxis = d3.svg.axis().scale(yScale).orient('left')
+    .ticks(6)
+    .tickFormat(function (d, i) {
+        return ['1', '2', '3', '4', '5', '6', '7', '8'][d];
+    });
 
   // Create the Axis for the graph.
   scatter_results.append("g")
@@ -192,8 +203,8 @@ function drawScatter(day_results, weather){
     .attr("class", "dot")
     .attr("rx", 3)
     .attr("ry", function (d) { return d.radius*1.5; })
-    .attr("cx", function(d) { return x(d.startTijd); })
-    .attr("cy", function(d) { return y(d.baan); })
+    .attr("cx", function(d) { return xScale(d.startTijd); })
+    .attr("cy", function(d) { return yScale(d.baan); })
     .style("fill", "red");
 
   // append rectangle over entire SVG for correct handling of mouse events.
@@ -201,10 +212,152 @@ function drawScatter(day_results, weather){
     .attr("width", width)
     .attr("height", height)
     .attr("opacity", "0")
+  eventlistener();
+return xScale, yScale;
 });
 }
 
 var movement;
-function eventlistener(){
-  var svg = d3.select
+var update;
+var bisectDate = d3.bisector(function(d) { return d.startTijd; }).left;
+function eventlistener(event){
+  var margin = {top: 20, right: 20, bottom: 30, left: 50},
+  width = 1760 - margin.left - margin.right,
+  height = 600  - margin.top - margin.bottom;
+  var padding = 50;
+  var left_pad = 100;
+  var xDomain = d3.extent(day_results, function(d) { return d.startTijd; })
+  var yDomain = d3.extent(day_results, function(d) { return d.baan; });
+  var xScale = d3.time.scale().range([padding, width - padding]).domain(xDomain);
+  var yScale = d3.scale.linear().range([padding, height - padding*2]).domain(yDomain);
+
+  var svg = d3.select("svg")
+  svg.on('mousemove', function() {
+    coordinates = d3.mouse(this);
+    var xsvg = coordinates[0]
+    var ysvg = coordinates[1]
+    // Trace which time point the mouse is closest to.
+    var mouseTime = xScale.invert(xsvg);
+    var i = bisectDate(day_results, mouseTime);
+    var t0 = day_results[i - 1]
+    var t1 = day_results[i]
+    
+    // work out which time value is closest to the mouse
+    var t = mouseTime - t0[1] > t1[1] - mouseTime ? t1 : t0;
+    var x = xScale(t.startTijd);
+    var y = yScale(t.baan);
+    updateTooltip(i,x,day_results);
+    mouseMoved();
+  });
+  cancelTimeout(event);
+  svg.on("mouseout", function(){
+    mouseOut(event);
+  });
+}
+function mouseOut(event){
+    var div = d3.select("#tooltip")
+    .style("opacity", "0")
+    cancelTimeout(event);
+    div.transition()        
+    .duration(500)      
+    .style("opacity", 0);   
+}
+
+function cancelTimeout(){
+  console.log("clearing timeout")
+  clearTimeout(movement);
+  clearTimeout(update);
+}
+
+function mouseMoved(event){
+  // Reset the opacity to zero.
+  var div = d3.select("#tooltip")
+    .attr("width", "200px")
+    .attr("height", "400px")
+    .style("opacity", "0");
+  // place the tooltip next to the mouse.
+  div.style("left", (d3.event.pageX) + "px")
+    .style("top", (d3.event.pageY - 200) + "px");
+  movement = setTimeout(function(){
+    showTooltip(event);
+  }, 1000);
+  update = setTimeout(function(){
+  updateTooltip(event);
+  }, 1000);
+
+}
+// Set the opacity of the tooltip to 70%
+function showTooltip(event){
+  var div = d3.select("#tooltip")
+    .style("opacity", "0.7")
+}
+
+function updateTooltip(i,x,day_results){
+  heat = []
+  d = day_results[i]
+  starttijd = d.startTijd
+  veld = d[2]
+  for (var x = 0; x < 6; x+=1) {
+    ploeg_heat = day_results[i-x]
+    ploeg2_heat = day_results[i+x]
+    if (ploeg_heat[2] === veld)
+      crewExists(ploeg_heat, heat)
+    if (ploeg2_heat[2] === veld)
+      addCrew(ploeg2_heat, heat)
+  }
+  for (test = 0; test < heat.length; test++){
+    ploeg_overview = heat[test]
+    position = ploeg_overview[5]["results"]["finish"]["position"]
+    console.log(position)
+    if (position == 1)
+      var tijd = ploeg_overview[5]["results"]["finish"]["time"]
+    console.log(tijd)
+    lane = ploeg_overview[4]
+    if (lane == 1)
+      var baan1 = ploeg_overview[3]
+    if (lane == 2)
+      var baan2 = ploeg_overview[3]
+    if (lane == 3)
+      var baan3 = ploeg_overview[3]
+    if (lane == 4)
+      var baan4 = ploeg_overview[3]
+    if (lane == 5)
+      var baan5 = ploeg_overview[3]
+    if (lane == 6)
+      var baan6 = ploeg_overview[3]
+    if (lane == 7)
+      var baan7 = ploeg_overview[3]
+    if (lane == 8)
+      var baan8 = ploeg_overview[3]
+  }
+  
+  var div = d3.select("#veld_tooltip")
+  div.html(veld)
+  var div = d3.select("#tijd_tooltip")
+  div.html("<br>" + "Winnende tijd: " + tijd  + "<br>")
+
+  var div = d3.select("#banen_tooltip")
+  div.html("<br>" + "Baan 1: " + baan1 + "<br>"
+    + "Baan 2: " + baan2 + "<br>"
+    + "Baan 3: " + baan3 + "<br>"
+    + "Baan 4: " + baan4 + "<br>"
+    + "Baan 5: " + baan5 + "<br>"
+    + "Baan 6: " + baan6 + "<br>"
+    + "Baan 7: " + baan7 + "<br>"
+    + "Baan 8: " + baan8 + "<br>")
+
+
+
+function crewExists(crewname, arr) {
+  return arr.some(function(el) {
+    return el.crewname === crewname;
+  }); 
+}
+function addCrew(crewname, arr) {
+  if (crewExists(crewname, arr)) {
+    return false; 
+  }
+  arr.push(crewname)
+  return true;
+}
 }
